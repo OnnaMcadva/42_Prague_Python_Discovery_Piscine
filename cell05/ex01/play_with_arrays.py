79062dccbc0fd71031b8a9059f2b434f5b6dc8c9
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2024-09-05 20:08:41 +0200

    Add files via upload

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -4,5 +4,5 @@ original_array = ([2, 8, 9, 48, 8, 22, -12, 2])
 
 new_array = [x + 2 for x in original_array]
 
-print("Original array:", original_array)
-print("New array:", new_array)
+print("\033[38;5;051m" + "Original array:", original_array)
+print("\033[38;5;118m" + "New array:", new_array)
--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python3
 
+import sys
+
+if len(sys.argv) != 2:
+    print("\033[38;5;227m" + "none")
+else:
+    parameter = sys.argv[1]
+    
+    user_input = input("\033[38;5;051m" + "What was the parameter? " + "\033[38;5;206m")
+    
+    if user_input == parameter:
+        print("\033[38;5;222m" + "Good job!")
+    else:
+        print("\033[38;5;118m" + "Nope, sorry...")
+
+# ?> ./parameter_matching.py
+# none
+# ?> ./parameter_matching.py "Hello"
+# What was the parameter? Bonjour
+# Nope, sorry...
+# ?> ./parameter_matching.py "Hello"
+# What was the parameter? Hello
+# Good job!
+# ?>
+
+# ORANGE      = \033[38;5;222m
+# GREEN_BR    = \033[38;5;118m
+# YELLOW_BR   = \033[38;5;227m
+# PINK_BR     = \033[38;5;206m
+# BLUE_BR     = \033[38;5;051m
+# PINK_BRR     = \033[38;5;219m
