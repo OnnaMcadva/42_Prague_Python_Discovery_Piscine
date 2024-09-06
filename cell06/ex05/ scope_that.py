b231b1bcd469f57c799f59b19597d5bc9dead0e6
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2024-09-06 12:55:37 +0200

    Delete cell06/ex05/ scope_that.py

--- cell06/ex05/ scope_that.py
+++
@@ -1,9 +0,0 @@
-
-
-
-• Inside the program, create a method called add_one that takes a parameter and
-adds 1 to the parameter passed to the method.
-• Initialize a variable in the body of the program, display it, and then call the method
-that adds 1.
-• Display your variable again in the body of the program.
-• What do you observe?
